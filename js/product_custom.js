bbdde8149ff5e8bf9bf9364c7024c70b4b038679
Oleh Petraniuk <olehshort@gmail.com>
2019-08-06 12:17:21 +0200

    Create Product custom js file: Init Quantity

--- js/product_custom.js
+++ js/product_custom.js
@@ -311,4 +311,41 @@ $(document).ready(function()
 				});
 			}
 		}
+    }
+    
+    /* 
+
+	7. Init Quantity
+
+	*/
+
+	function initQuantity()
+	{
+		// Handle product quantity input
+		if($('.product_quantity').length)
+		{
+			var input = $('#quantity_input');
+			var incButton = $('#quantity_inc_button');
+			var decButton = $('#quantity_dec_button');
+
+			var originalVal;
+			var endVal;
+
+			incButton.on('click', function()
+			{
+				originalVal = input.val();
+				endVal = parseFloat(originalVal) + 1;
+				input.val(endVal);
+			});
+
+			decButton.on('click', function()
+			{
+				originalVal = input.val();
+				if(originalVal > 0)
+				{
+					endVal = parseFloat(originalVal) - 1;
+					input.val(endVal);
+				}
+			});
+		}
 	}
