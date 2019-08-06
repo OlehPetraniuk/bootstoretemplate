8bab597312866e1ede9f9fb797be40b26639a8e7
Oleh Petraniuk <olehshort@gmail.com>
2019-08-06 11:09:38 +0200

    Create Product custom js file: Vars and Inits, Set Header

---
+++ js/product_custom.js
@@ -0,0 +1,79 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Custom Dropdown
+4. Init Page Menu
+5. Init Recently Viewed Slider
+6. Init Brands Slider
+7. Init Quantity
+8. Init Color
+9. Init Favorites
+10. Init Image
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var menuActive = false;
+	var header = $('.header');
+
+	setHeader();
+
+	initCustomDropdown();
+	initPageMenu();
+	initViewedSlider();
+	initBrandsSlider();
+	initQuantity();
+	initColor();
+	initFavs();
+	initImage();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		//To pin main nav to the top of the page when it's reached
+		//uncomment the following
+
+		// var controller = new ScrollMagic.Controller(
+		// {
+		// 	globalSceneOptions:
+		// 	{
+		// 		triggerHook: 'onLeave'
+		// 	}
+		// });
+
+		// var pin = new ScrollMagic.Scene(
+		// {
+		// 	triggerElement: '.main_nav'
+		// })
+		// .setPin('.main_nav').addTo(controller);
+
+		if(window.innerWidth > 991 && menuActive)
+		{
+			closeMenu();
+		}
+	}
