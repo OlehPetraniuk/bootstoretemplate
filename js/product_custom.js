06afc3c74d7832def591f2db3cc64e8cfdabca0a
Oleh Petraniuk <olehshort@gmail.com>
2019-08-06 11:57:53 +0200

    Create Product custom js file: Init Recently Viewed Slider

--- js/product_custom.js
+++ js/product_custom.js
@@ -217,4 +217,54 @@ $(document).ready(function()
 		var menuContent = $('.page_menu_content');
 		TweenMax.to(menuContent, 0.3, {height:0});
 		menuActive = false;
+    }
+    
+    /* 
+
+	5. Init Recently Viewed Slider
+
+	*/
+
+	function initViewedSlider()
+	{
+		if($('.viewed_slider').length)
+		{
+			var viewedSlider = $('.viewed_slider');
+
+			viewedSlider.owlCarousel(
+			{
+				loop:true,
+				margin:30,
+				autoplay:true,
+				autoplayTimeout:6000,
+				nav:false,
+				dots:false,
+				responsive:
+				{
+					0:{items:1},
+					575:{items:2},
+					768:{items:3},
+					991:{items:4},
+					1199:{items:6}
+				}
+			});
+
+			if($('.viewed_prev').length)
+			{
+				var prev = $('.viewed_prev');
+				prev.on('click', function()
+				{
+					viewedSlider.trigger('prev.owl.carousel');
+				});
+			}
+
+			if($('.viewed_next').length)
+			{
+				var next = $('.viewed_next');
+				next.on('click', function()
+				{
+					viewedSlider.trigger('next.owl.carousel');
+				});
+			}
+		}
 	}
