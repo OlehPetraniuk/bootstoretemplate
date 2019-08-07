19454ce6d66f96c199dc9bd973cdfaf9ff67e238
Oleh Petraniuk <olehshort@gmail.com>
2019-08-07 21:22:33 +0200

    Create Custom js file: Init Banner 2 Slider

--- js/custom.js
+++ js/custom.js
@@ -561,4 +561,27 @@ $(document).ready(function()
 				});
 			}
 		}
+    }
+    
+    /* 
+
+	12. Init Banner 2 Slider
+
+	*/
+
+	function initBanner2Slider()
+	{
+		if($('.banner_2_slider').length)
+		{
+			var banner2Slider = $('.banner_2_slider');
+			banner2Slider.owlCarousel(
+			{
+				items:1,
+				loop:true,
+				nav:false,
+				dots:true,
+				dotsContainer: '.banner_2_dots',
+				smartSpeed:1200
+			});
+		}
 	}
