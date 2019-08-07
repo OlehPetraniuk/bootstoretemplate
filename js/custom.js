ee1cf51bd73daea52bbbd900d138e80029d5f312
Oleh Petraniuk <olehshort@gmail.com>
2019-08-07 21:03:09 +0200

    Create Custom js file: Init Featured Slider

--- js/custom.js
+++ js/custom.js
@@ -374,4 +374,97 @@ $(document).ready(function()
 				});
 			});
 		}
+    }
+    
+    /* 
+
+	8. Init Featured Slider
+
+	*/
+
+	function initFeaturedSlider()
+	{
+		if($('.featured_slider').length)
+		{
+			var featuredSliders = $('.featured_slider');
+			featuredSliders.each(function()
+			{
+				var featuredSlider = $(this);
+				initFSlider(featuredSlider);
+			});
+				
+		}
+	}
+
+	function initFSlider(fs)
+	{
+		var featuredSlider = fs;
+		featuredSlider.on('init', function()
+		{
+			var activeItems = featuredSlider.find('.slick-slide.slick-active');
+			for(var x = 0; x < activeItems.length - 1; x++)
+			{
+				var item = $(activeItems[x]);
+				item.find('.border_active').removeClass('active');
+				if(item.hasClass('slick-active'))
+				{
+					item.find('.border_active').addClass('active');
+				}
+			}
+		}).on(
+		{
+			afterChange: function(event, slick, current_slide_index, next_slide_index)
+			{
+				var activeItems = featuredSlider.find('.slick-slide.slick-active');
+				activeItems.find('.border_active').removeClass('active');
+				for(var x = 0; x < activeItems.length - 1; x++)
+				{
+					var item = $(activeItems[x]);
+					item.find('.border_active').removeClass('active');
+					if(item.hasClass('slick-active'))
+					{
+						item.find('.border_active').addClass('active');
+					}
+				}
+			}
+		})
+		.slick(
+		{
+			rows:2,
+			slidesToShow:4,
+			slidesToScroll:4,
+			infinite:false,
+			arrows:false,
+			dots:true,
+			responsive:
+			[
+				{
+					breakpoint:768, settings:
+					{
+						rows:2,
+						slidesToShow:3,
+						slidesToScroll:3,
+						dots:true
+					}
+				},
+				{
+					breakpoint:575, settings:
+					{
+						rows:2,
+						slidesToShow:2,
+						slidesToScroll:2,
+						dots:false
+					}
+				},
+				{
+					breakpoint:480, settings:
+					{
+						rows:1,
+						slidesToShow:1,
+						slidesToScroll:1,
+						dots:false
+					}
+				}
+			]
+		});
 	}
