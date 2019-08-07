4a5046e760829f8a65a6f89616081db6c357fffb
Oleh Petraniuk <olehshort@gmail.com>
2019-08-07 20:21:21 +0200

    Create Custom js file: Table of Contents & Vars and Inits

---
+++ js/custom.js
@@ -0,0 +1,70 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Custom Dropdown
+4. Init Page Menu
+5. Init Deals Slider
+6. Init Tab Lines
+7. Init Tabs
+8. Init Featured Slider
+9. Init Favorites
+10. Init ZIndex
+11. Init Popular Categories Slider
+12. Init Banner 2 Slider
+13. Init Arrivals Slider
+14. Init Arrivals Slider ZIndex
+15. Init Best Sellers Slider
+16. Init Trends Slider
+17. Init Reviews Slider
+18. Init Recently Viewed Slider
+19. Init Brands Slider
+20. Init Timer
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var menuActive = false;
+	var header = $('.header');
+
+	setHeader();
+
+	initCustomDropdown();
+	initPageMenu();
+	initDealsSlider();
+	initTabLines();
+	initFeaturedSlider();
+	featuredSliderZIndex();
+	initPopularSlider();
+	initBanner2Slider();
+	initFavs();
+	initArrivalsSlider();
+	arrivalsSliderZIndex();
+	bestsellersSlider();
+	initTabs();
+	initTrendsSlider();
+	initReviewsSlider();
+	initViewedSlider();
+	initBrandsSlider();
+	initTimer();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+		featuredSliderZIndex();
+		initTabLines();
+	});
