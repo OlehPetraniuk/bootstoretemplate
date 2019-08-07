4308adbe721798f86e9bc4c35df44c4ec1004f34
Oleh Petraniuk <olehshort@gmail.com>
2019-08-07 20:37:25 +0200

    Create Custom js file: Init Deals Slider

--- js/custom.js
+++ js/custom.js
@@ -239,4 +239,48 @@ $(document).ready(function()
 		var menuContent = $('.page_menu_content');
 		TweenMax.to(menuContent, 0.3, {height:0});
 		menuActive = false;
+    }
+    
+    /* 
+
+	5. Init Deals Slider
+
+	*/
+
+	function initDealsSlider()
+	{
+		if($('.deals_slider').length)
+		{
+			var dealsSlider = $('.deals_slider');
+			dealsSlider.owlCarousel(
+			{
+				items:1,
+				loop:false,
+				navClass:['deals_slider_prev', 'deals_slider_next'],
+				nav:false,
+				dots:false,
+				smartSpeed:1200,
+				margin:30,
+				autoplay:false,
+				autoplayTimeout:5000
+			});
+
+			if($('.deals_slider_prev').length)
+			{
+				var prev = $('.deals_slider_prev');
+				prev.on('click', function()
+				{
+					dealsSlider.trigger('prev.owl.carousel');
+				});	
+			}
+
+			if($('.deals_slider_next').length)
+			{
+				var next = $('.deals_slider_next');
+				next.on('click', function()
+				{
+					dealsSlider.trigger('next.owl.carousel');
+				});	
+			}
+		}
 	}
