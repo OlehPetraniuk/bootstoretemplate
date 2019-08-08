af0f102a9b84a9aae688a621443b2bfbe582b190
Oleh Petraniuk <olehshort@gmail.com>
2019-08-08 16:15:08 +0200

    Create SHop Custom js file: Vars and Inits

---
+++ js/shop_custom.js
@@ -0,0 +1,46 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Custom Dropdown
+4. Init Page Menu
+5. Init Recently Viewed Slider
+6. Init Brands Slider
+7. Init Isotope
+8. Init Price Slider
+9. Init Favorites
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var menuActive = false;
+	var header = $('.header');
+
+	setHeader();
+
+	initCustomDropdown();
+	initPageMenu();
+	initViewedSlider();
+	initBrandsSlider();
+	initIsotope();
+	initPriceSlider();
+	initFavs();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
