9717cd534cec3a2a10e4f1d2e7dfae106aaf2fea
Oleh Petraniuk <olehshort@gmail.com>
2019-08-08 15:22:02 +0200

    Create Custom js file: Init Recently Viewed Slider

--- js/custom.js
+++ js/custom.js
@@ -853,4 +853,54 @@ $(document).ready(function()
 				}
 			});
 		}
+    }
+    
+    /* 
+
+	18. Init Recently Viewed Slider
+
+	*/
+
+	function initViewedSlider()
+	{
+		if($('.viewed_slider').length)
+		{
+			var viewedSlider = $('.viewed_slider');
+
+			viewedSlider.owlCarousel(
+			{
+				loop:true,
+				margin:30,
+				autoplay:true,
+				autoplayTimeout:6000,
+				nav:false,
+				dots:false,
+				responsive:
+				{
+					0:{items:1},
+					575:{items:2},
+					768:{items:3},
+					991:{items:4},
+					1199:{items:6}
+				}
+			});
+
+			if($('.viewed_prev').length)
+			{
+				var prev = $('.viewed_prev');
+				prev.on('click', function()
+				{
+					viewedSlider.trigger('prev.owl.carousel');
+				});
+			}
+
+			if($('.viewed_next').length)
+			{
+				var next = $('.viewed_next');
+				next.on('click', function()
+				{
+					viewedSlider.trigger('next.owl.carousel');
+				});
+			}
+		}
 	}
