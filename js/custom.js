96dc6f05c6a5fc78284b0fab3432d01fd2690808
Oleh Petraniuk <olehshort@gmail.com>
2019-08-08 11:26:44 +0200

    Create Custom js file: Init Best Sellers SLider

--- js/custom.js
+++ js/custom.js
@@ -703,4 +703,71 @@ $(document).ready(function()
 				$('.arrivals_slider .slick-dots').css('display', "block");
 			});
 		}
+    }
+    
+    /* 
+
+	15. Init Best Sellers Slider
+
+	*/
+
+	function bestsellersSlider()
+	{
+		if($('.bestsellers_slider').length)
+		{
+			var bestsellersSliders = $('.bestsellers_slider');
+			bestsellersSliders.each(function()
+			{
+				var bestsellersSlider = $(this);
+
+				initBSSlider(bestsellersSlider);
+			})	
+		}
+	}
+
+	function initBSSlider(bss)
+	{
+		var bestsellersSlider = bss;
+
+		bestsellersSlider.slick(
+		{
+			rows:2,
+			infinite:true,
+			slidesToShow:3,
+			slidesToScroll:3,
+			arrows:false,
+			dots:true,
+			autoplay: true,
+  			autoplaySpeed: 6000,
+			responsive:
+			[
+				{
+					breakpoint:1199, settings:
+					{
+						rows:2,
+						slidesToShow:2,
+						slidesToScroll:2,
+						dots:true
+					}
+				},
+				{
+					breakpoint:991, settings:
+					{
+						rows:2,
+						slidesToShow:1,
+						slidesToScroll:1,
+						dots:true
+					}
+				},
+				{
+					breakpoint:575, settings:
+					{
+						rows:1,
+						slidesToShow:1,
+						slidesToScroll:1,
+						dots:false
+					}
+				}
+			]
+		});
 	}
